df520959a0436f30a1b80c148e46dcccb39d02ef
Matt Morrison <mattlmorrison1@gmail.com>
2026-08-11 08:30:41 +0100

    add clear filters

--- src/pages/PaymentsPage.tsx
+++ src/pages/PaymentsPage.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent, useState } from "react";
-import { Container, SearchButton, SearchInput, Spinner, StatusBadge, TableCell, Title } from "../components/components";
+import { Container, FilterRow, SearchButton, SearchInput, Spinner, StatusBadge, TableCell, Title, ClearButton } from "../components/components";
 import { useFetchPayments } from "../hooks/useFetchPayments";
 import { Table } from "../components/Table";
 import { Payment } from "../types/payment";
@@ -33,18 +33,31 @@ export const PaymentsPage = () => {
     setSearch(searchTerm.trim());
   };
 
+  const showClear = search != '';
+
+  const resetFilters = () => {
+    setSearchTerm(''); 
+    setSearch('');
+  }
+
   return (
     <Container>
       <Title>{I18N.PAGE_TITLE}</Title>
 
       <form onSubmit={handleSubmit}>
-        <SearchInput
-          aria-label={I18N.SEARCH_LABEL}
-          placeholder={I18N.SEARCH_PLACEHOLDER}
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-        />
-        <SearchButton type="submit">{I18N.SEARCH_BUTTON}</SearchButton>
+        <FilterRow>
+          <SearchInput
+            aria-label={I18N.SEARCH_LABEL}
+            placeholder={I18N.SEARCH_PLACEHOLDER}
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+          />
+          <SearchButton type="submit">{I18N.SEARCH_BUTTON}</SearchButton>
+          {showClear &&
+            <ClearButton type="button" onClick={resetFilters}>
+              {I18N.CLEAR_FILTERS}
+            </ClearButton>}
+        </FilterRow>
       </form>
 
       {paymentData ? (
